e6049e330093821eed8c261cc7acb493c0e41c59
Sofiya <selyukin@yahoo.com>
2017-07-24 21:08:22 -0400

    cleaned script, some functionality broken cause of IDs

--- app/products_app.py
+++ app/products_app.py
@@ -14,8 +14,12 @@ inventory = []
 
 with open(csv_path, "r") as file:
 		reader = csv.DictReader(file)
-		for row in reader:
-			inventory.append(row)
+		for ordered_dict in reader:
+			inventory.append(dict(ordered_dict))
+
+ids = []
+for i in range(0, len(inventory)):
+	ids.append(inventory[i]["id"])
 
 # Created a function for writing the dictionary back to CSV
 def WriteCSV():
@@ -61,9 +65,6 @@ def ShowOp():
 	product ID is not in the inventory the app states so 
 	and prompts the user for a new ID.'''
 	item = input("Please specify a product id:")
-	ids = []
-	for i in range(0, len(inventory)):
-		ids.append(inventory[i]["id"])
 	if item in ids:
 		item = int(item) - 1
 		print ("Name:", inventory[int(item)]["name"], "\n",
@@ -85,6 +86,7 @@ def CreateOp():
 	newAisle = input("Aisle:")
 	newPrice = float(input("Price:"))
 	print ("Creating a new product.")
+	#can modify below to autoincrement the product IDs, should re-index
 	inventory.append({'id':int(len(inventory)+1), 'name':newName,
 		'department':newDept, 'aisle':newAisle, 'price':newPrice})
 	WriteCSV()
@@ -116,37 +118,17 @@ def UpdateOp():
 			"Aisle:", inventory[int(item)]["aisle"], "\n",
 			"Price: $" + str(inventory[int(item)]["price"]))
 
-'''
-#The below functions still utilize the dictionary rather than the CSV
-products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-]
-'''
-
-#Will need to delete from csv
 def DestroyOp():
+	'''This function prompts the user for a
+	product ID and then deletes that product
+	information from the dictionary. The CSV
+	is then overwritten with the updated 
+	dictionary.
+	'''
 	item = input("Please specify a product to remove:")
 	item = int(item) - 1
 	inventory.remove(inventory[item])
+	#should reindex products
 	print ("Product deleted. There are now {0} products in the inventory"
 		.format(len(inventory)))
 	WriteCSV()
